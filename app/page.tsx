97e267d5cec25db9b0bbc61b7fc228a221845271
md-samrat <md.samrat61790@gmail.com>
2026-07-05 17:06:39 +0600

    fetch product data

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,39 @@
 import prisma from "@/lib/prisma";
-import Image from "next/image";
 
 export default async function Home() {
-  const users = await prisma.user.findMany();
+  const products = await prisma.product.findMany();
+
   return (
-    <div>
-      {users.map((user) => (
-        <div key={user.id}>
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-        </div>
-      ))}
+    <div className="max-w-6xl mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-6">Product List</h1>
+
+      <table className="w-full border border-gray-300">
+        <thead className="bg-green-500">
+          <tr>
+            <th className="border p-3">SL</th>
+            <th className="border p-3">Name</th>
+            <th className="border p-3">Description</th>
+            <th className="border p-3">Price</th>
+            <th className="border p-3">Created At</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {products.map((product, index) => (
+            <tr key={product.id}>
+              <td className="border p-3">{index + 1}</td>
+              <td className="border p-3">{product.name}</td>
+              <td className="border p-3">
+                {product.description ?? "N/A"}
+              </td>
+              <td className="border p-3">${product.price}</td>
+              <td className="border p-3">
+                {product.createdAt.toLocaleDateString()}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
